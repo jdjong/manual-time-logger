0dc04e9ac33d5043f9e49b45283bec591ffc91e8
Joost de Jong <joostdejong@gmail.com>
2020-03-31 13:17:16 +0200

    Saving report entry change
    - 0 values are not filled, left empty for better readability
    - Values are rounded to two decimals for better readability.

--- ManualTimeLogger.ReportBuilder/ReportCsvFileRepository.cs
+++ ManualTimeLogger.ReportBuilder/ReportCsvFileRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,46 +7,53 @@ namespace ManualTimeLogger.ReportBuilder
 {
     public class ReportCsvFileRepository : IReportCsvFileRepository
     {
-    public char CsvSeparator => ';';
-    private readonly string _basePath;
-    private readonly string _fileName;
-    private string FullFilePath => Path.Combine(_basePath, _fileName);
+        public char CsvSeparator => ';';
+        private readonly string _basePath;
+        private readonly string _fileName;
+        private string FullFilePath => Path.Combine(_basePath, _fileName);
 
-    public ReportCsvFileRepository(string basePath, string fileName)
-    {
-        if (string.IsNullOrEmpty(basePath)) throw new ArgumentNullException(basePath);
-        if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(fileName);
-
-        _basePath = basePath;
-        _fileName = fileName;
-
-        if (!Directory.Exists(_basePath))
+        public ReportCsvFileRepository(string basePath, string fileName)
         {
-            Directory.CreateDirectory(_basePath);
-        }
+            if (string.IsNullOrEmpty(basePath)) throw new ArgumentNullException(basePath);
+            if (string.IsNullOrEmpty(fileName)) throw new ArgumentNullException(fileName);
 
-        if (File.Exists(FullFilePath))
-        {
-            File.Delete(FullFilePath);
-        }
-    }
+            _basePath = basePath;
+            _fileName = fileName;
 
-    // TODO, it is strange that the calling party should know and create the header with number of columns matching the number of columns created in the save report entry method.
-    public void CreateHeader(string[] header)
-    {
-        File.AppendAllLines(
-            FullFilePath,
-            header);
-    }
-
-    public void SaveReportEntry(ReportEntry reportEntry)
-    {
-        File.AppendAllLines(
-            FullFilePath,
-            new[]
+            if (!Directory.Exists(_basePath))
             {
-                $"\"{reportEntry.Engineer}\"{CsvSeparator}\"{reportEntry.Description}\"{CsvSeparator}\"{reportEntry.NrOfHoursPerDay.Sum(x => x.Value)}\"{CsvSeparator}{string.Join(CsvSeparator.ToString(), reportEntry.NrOfHoursPerDay.Select(x => $"\"{x.Value}\""))}"
-            });
-    }
+                Directory.CreateDirectory(_basePath);
+            }
+
+            if (File.Exists(FullFilePath))
+            {
+                File.Delete(FullFilePath);
+            }
+        }
+
+        // TODO, it is strange that the calling party should know and create the header with number of columns matching the number of columns created in the save report entry method.
+        public void CreateHeader(string[] header)
+        {
+            File.AppendAllLines(
+                FullFilePath,
+                header);
+        }
+
+        public void SaveReportEntry(ReportEntry reportEntry)
+        {
+            File.AppendAllLines(
+                FullFilePath,
+                new[]
+                {
+                    $"\"{reportEntry.Engineer}\"{CsvSeparator}\"{reportEntry.Description}\"{CsvSeparator}\"{reportEntry.NrOfHoursPerDay.Sum(x => x.Value)}\"{CsvSeparator}{string.Join(CsvSeparator.ToString(), reportEntry.NrOfHoursPerDay.Select(hoursForDay => $"\"{GetHoursForDayString(hoursForDay)}\""))}"
+                });
+        }
+
+        private string GetHoursForDayString(KeyValuePair<DateTime, float> hoursForDay)
+        {
+            return hoursForDay.Value < 0.01f
+                ? string.Empty
+                : Math.Round(hoursForDay.Value, 2, MidpointRounding.AwayFromZero).ToString("0.00");
+        }
     }
 }
