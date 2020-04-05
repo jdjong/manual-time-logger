126ab295c8b518f0034230da0a974ba35ec93ec4
Joost de Jong <joostdejong@gmail.com>
2020-04-05 07:18:53 +0200

    Created command handler
    - Added some to do's

---
+++ ManualTimeLogger.ReportBuilder/CommandHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ManualTimeLogger.Domain;
+using ManualTimeLogger.Persistence;
+using ManualTimeLogger.ReportBuilder.Commands;
+using ManualTimeLogger.ReportBuilder.ReportSets;
+
+namespace ManualTimeLogger.ReportBuilder
+{
+    public class CommandHandler
+    {
+        private readonly string _timeLogsBasePath;
+        private readonly string _reportsBasePath;
+
+        // TODO, add big integration test based on an example time log and generated reports which are tested ok. Test should check if time log generates expected reports.
+        // TODO, remove account filter logic from report builders (deep down) and move it here.
+
+        public CommandHandler(string timeLogsBasePath, string reportsBasePath)
+        {
+            _timeLogsBasePath = timeLogsBasePath;
+            _reportsBasePath = reportsBasePath;
+        }
+
+        private void Handle(BuildWeekReportsCommand command)
+        {
+            var logEntriesPerEngineer = GetLogEntriesPerEngineer();
+            var logEntriesPerLabel = GetLogEntriesPerLabel(logEntriesPerEngineer);
+            var logEntriesPerDay = GetLogEntriesPerDay(logEntriesPerEngineer);
+
+            var perEngineerWeekReportSet = new PerEngineerWeekReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+            var perLabelWeekReportSet = new PerLabelWeekReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+            var cumulativeWeekReportSet = new CumulativeWeekReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+
+            perEngineerWeekReportSet.Create(logEntriesPerEngineer);
+            perLabelWeekReportSet.Create(logEntriesPerLabel);
+            cumulativeWeekReportSet.Create(logEntriesPerDay);
+        }
+
+        public void Handle(BuildMonthReportsCommand command)
+        {
+            var logEntriesPerEngineer = GetLogEntriesPerEngineer();
+            var logEntriesPerLabel = GetLogEntriesPerLabel(logEntriesPerEngineer);
+            var logEntriesPerDay = GetLogEntriesPerDay(logEntriesPerEngineer);
+
+            var perEngineerMonthReportSet = new PerEngineerMonthReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+            var perLabelMonthReportSet = new PerLabelMonthReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+            var cumulativeMonthReportSet = new CumulativeMonthReportSet(_reportsBasePath, command.FromDay, command.AccountFilter);
+
+            perEngineerMonthReportSet.Create(logEntriesPerEngineer);
+            perLabelMonthReportSet.Create(logEntriesPerLabel);
+            cumulativeMonthReportSet.Create(logEntriesPerDay);
+        }
+
+        private Dictionary<string, IEnumerable<LogEntry>> GetLogEntriesPerLabel(Dictionary<string, IEnumerable<LogEntry>> logEntriesPerEngineer)
+        {
+            var logEntriesPerLabel = logEntriesPerEngineer.SelectMany(x => x.Value).GroupBy(x => x.Label);
+            return logEntriesPerLabel.ToDictionary(grouping => grouping.Key, grouping => grouping.Select(logEntry => logEntry));
+        }
+
+        private IEnumerable<IGrouping<DateTime, LogEntry>> GetLogEntriesPerDay(Dictionary<string, IEnumerable<LogEntry>> logEntriesPerEngineer)
+        {
+            return logEntriesPerEngineer.SelectMany(x => x.Value).GroupBy(x => x.CreateDate);
+        }
+
+        private Dictionary<string, IEnumerable<LogEntry>> GetLogEntriesPerEngineer()
+        {
+            var allTimeLogRepositories = GetAllTimeLogRepositories();
+
+            return allTimeLogRepositories
+                .GroupBy(repo => repo.GetEngineerName())
+                .ToDictionary(grouping => grouping.Key, grouping => grouping.SelectMany(repo => repo.GetAllLogEntries()));
+        }
+
+        private List<CsvFileRepository> GetAllTimeLogRepositories()
+        {
+            var allTimeLogFileNames = Directory.EnumerateFiles(_timeLogsBasePath);
+            var allTimeLogRepositories = allTimeLogFileNames.Select(filePath => new CsvFileRepository(_timeLogsBasePath, Path.GetFileName(filePath))).ToList();
+            return allTimeLogRepositories;
+        }
+    }
+}
--- ManualTimeLogger.ReportBuilder/Program.cs
+++ ManualTimeLogger.ReportBuilder/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using ManualTimeLogger.Domain;
-using ManualTimeLogger.Persistence;
-using ManualTimeLogger.ReportBuilder.Commands;
-using ManualTimeLogger.ReportBuilder.ReportSets;
+﻿using ManualTimeLogger.ReportBuilder.Commands;
 
 namespace ManualTimeLogger.ReportBuilder
 {
@@ -13,66 +6,10 @@ namespace ManualTimeLogger.ReportBuilder
     {
         static void Main(string[] args)
         {
+            var commandHandler = new CommandHandler(Properties.Settings.Default.TimeLogsBasePath, Properties.Settings.Default.ReportsBasePath);
             var reportingCommandProvider = new CommandProvider();
-            Handle((dynamic)reportingCommandProvider.GetCommand(args));
-        }
 
-        private static void Handle(BuildWeekReportsCommand command)
-        {
-            var logEntriesPerEngineer = GetLogEntriesPerEngineer();
-            var logEntriesPerLabel = GetLogEntriesPerLabel(logEntriesPerEngineer);
-            var logEntriesPerDay = GetLogEntriesPerDay(logEntriesPerEngineer);
-
-            var perEngineerWeekReportSet = new PerEngineerWeekReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-            var perLabelWeekReportSet = new PerLabelWeekReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-            var cumulativeWeekReportSet = new CumulativeWeekReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-            
-            perEngineerWeekReportSet.Create(logEntriesPerEngineer);
-            perLabelWeekReportSet.Create(logEntriesPerLabel);
-            cumulativeWeekReportSet.Create(logEntriesPerDay);
-        }
-
-        private static void Handle(BuildMonthReportsCommand command)
-        {
-            var logEntriesPerEngineer = GetLogEntriesPerEngineer();
-            var logEntriesPerLabel = GetLogEntriesPerLabel(logEntriesPerEngineer);
-            var logEntriesPerDay = GetLogEntriesPerDay(logEntriesPerEngineer);
-
-            var perEngineerMonthReportSet = new PerEngineerMonthReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-            var perLabelMonthReportSet = new PerLabelMonthReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-            var cumulativeMonthReportSet = new CumulativeMonthReportSet(Properties.Settings.Default.ReportsBasePath, command.FromDay, command.AccountFilter);
-
-            perEngineerMonthReportSet.Create(logEntriesPerEngineer);
-            perLabelMonthReportSet.Create(logEntriesPerLabel);
-            cumulativeMonthReportSet.Create(logEntriesPerDay);
-        }
-
-        private static Dictionary<string, IEnumerable<LogEntry>> GetLogEntriesPerLabel(Dictionary<string, IEnumerable<LogEntry>> logEntriesPerEngineer)
-        {
-            var logEntriesPerLabel = logEntriesPerEngineer.SelectMany(x => x.Value).GroupBy(x => x.Label);
-            return logEntriesPerLabel.ToDictionary(grouping => grouping.Key, grouping => grouping.Select(logEntry => logEntry));
-        }
-
-        private static IEnumerable<IGrouping<DateTime, LogEntry>> GetLogEntriesPerDay(Dictionary<string, IEnumerable<LogEntry>> logEntriesPerEngineer)
-        {
-            return logEntriesPerEngineer.SelectMany(x => x.Value).GroupBy(x => x.CreateDate);
-        }
-
-        private static Dictionary<string, IEnumerable<LogEntry>> GetLogEntriesPerEngineer()
-        {
-            var allTimeLogRepositories = GetAllTimeLogRepositories();
-
-            return allTimeLogRepositories
-                .GroupBy(repo => repo.GetEngineerName())
-                .ToDictionary(grouping => grouping.Key, grouping => grouping.SelectMany(repo => repo.GetAllLogEntries()));
-        }
-
-        private static List<CsvFileRepository> GetAllTimeLogRepositories()
-        {
-            var timeLogsBasePath = Properties.Settings.Default.TimeLogsBasePath;
-            var allTimeLogFileNames = Directory.EnumerateFiles(timeLogsBasePath);
-            var allTimeLogRepositories = allTimeLogFileNames.Select(filePaths => new CsvFileRepository(timeLogsBasePath, Path.GetFileName(filePaths))).ToList();
-            return allTimeLogRepositories;
+            commandHandler.Handle((dynamic)reportingCommandProvider.GetCommand(args));
         }
     }
 }
